28cc9c730230aa82b27fca9f021156d20e849720
Pedro Henrique Penna <ppenna@microsoft.com>
2022-02-01 11:20:08 -0800

    [queue] Feature: Queue Module

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,11 @@
 #![feature(new_uninit)]
 #![feature(test)]
 
+#[macro_use]
+extern crate derive_more;
+
 pub mod fail;
 pub mod memory;
+pub mod queue;
 
 pub use self::memory::Buffer as RuntimeBuf;

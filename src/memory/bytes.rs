5dc6825d946c7539a76ec551583324110cb07340
Pedro Henrique Penna <ppenna@microsoft.com>
2022-02-10 04:55:40 -0800

    [memory] Enhancement: Refactoring Unit Tests

--- src/memory/bytes.rs
+++ src/memory/bytes.rs
@@ -111,3 +111,31 @@ impl Buffer for Bytes {
         self.len -= nbytes;
     }
 }
+
+//==============================================================================
+// Unit Tests
+//==============================================================================
+
+#[cfg(test)]
+mod tests {
+    use super::{Buffer, Bytes};
+    use std::sync::Arc;
+
+    /// Tests for buffer adjust.
+    #[test]
+    fn buf_adjust() {
+        let data: [u8; 4] = [1, 2, 3, 4];
+        let mut buf: Bytes = Bytes::new(Some(Arc::new(data)), 0, 4);
+        buf.adjust(2);
+        assert_eq!(*buf, data[2..]);
+    }
+
+    /// Tests for buffer trim.
+    #[test]
+    fn buf_trim() {
+        let data: [u8; 4] = [1, 2, 3, 4];
+        let mut buf: Bytes = Bytes::new(Some(Arc::new(data)), 0, 4);
+        buf.trim(2);
+        assert_eq!(*buf, data[..2]);
+    }
+}

ab79b7804e23fd269391f6360ac5cac3bc093599
Pedro Henrique Penna <ppenna@microsoft.com>
2022-02-24 05:36:21 -0800

    [network] Feature: Port16 Data Type

--- src/network/types/mod.rs
+++ src/network/types/mod.rs
@@ -3,9 +3,10 @@
 
 mod ipv4;
 mod macaddr;
+mod portnum;
 
 //==============================================================================
 // Exports
 //==============================================================================
 
-pub use self::{ipv4::Ipv4Addr, macaddr::MacAddress};
+pub use self::{ipv4::Ipv4Addr, macaddr::MacAddress, portnum::Port16};
